33c3daf0148bf8d11a87263be4ab87131df68f4d
josephd81301-source <josephd81301@gmail.com>
2025-09-24 14:23:36 +0100

    second commit

---
+++ scripts/1_analysis.R
@@ -0,0 +1,10 @@
+#install tidyverse
+library(tidyverse)
+#read penguins
+penguins <- read.table
+#look at penguins
+head(penguins)
+#run a linear regression
+model1 <- lm(species ~ flipper_length_mm, data=penguins_raw)
+summary(model1)
+#something uselsssssssssss
--- scripts/1_script.R
+++
@@ -1,4 +0,0 @@
-library(tidyverse)
-read.table
-lm(penguins)
-head(penguins)
